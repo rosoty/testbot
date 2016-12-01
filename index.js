65b4909e659b50c6f10b67f39f5c9d1b262923bf
Mac <mac@Macs-MacBook-Pro.local>
2016-12-01 12:34:37 +0700

    new

--- index.js
+++ index.js
@@ -67,58 +67,107 @@ function kittenMessage(recipientId, text) {
             var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
             
             message = {
-                "attachment":{
-                    "type":"template",
-                    "payload":{
-                        "template_type":"receipt",
-                        "recipient_name":"Stephane Crozatier",
-                        "order_number":"12345678902",
-                        "currency":"USD",
-                        "payment_method":"Visa 2345",        
-                        "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
-                        "timestamp":"1428444852", 
-                        "elements":[
-                          {
-                            "title":"Classic White T-Shirt",
-                            "subtitle":"100% Soft and Luxurious Cotton",
-                            "quantity":2,
-                            "price":50,
-                            "currency":"USD",
-                            "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
-                          },
-                          {
-                            "title":"Classic Gray T-Shirt",
-                            "subtitle":"100% Soft and Luxurious Cotton",
-                            "quantity":1,
-                            "price":25,
-                            "currency":"USD",
-                            "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
-                          }
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "list",
+                        "elements": [
+                            {
+                                "title": "Classic T-Shirt Collection",
+                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                                "subtitle": "See all our colors",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "View",
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Classic White T-Shirt",
+                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+                                "subtitle": "100% Cotton, 200% Comfortable",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Shop Now",
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+                                    }
+                                ]                
+                            },
+                            {
+                                "title": "Classic Blue T-Shirt",
+                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                                "subtitle": "100% Cotton, 200% Comfortable",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Shop Now",
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+                                    }
+                                ]                
+                            },
+                            {
+                                "title": "Classic Black T-Shirt",
+                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
+                                "subtitle": "100% Cotton, 200% Comfortable",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Shop Now",
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+                                    }
+                                ]                
+                            }
                         ],
-                        "address":{
-                          "street_1":"1 Hacker Way",
-                          "street_2":"",
-                          "city":"Menlo Park",
-                          "postal_code":"94025",
-                          "state":"CA",
-                          "country":"US"
-                        },
-                        "summary":{
-                          "subtotal":75.00,
-                          "shipping_cost":4.95,
-                          "total_tax":6.19,
-                          "total_cost":56.14
-                        },
-                        "adjustments":[
-                          {
-                            "name":"New Customer Discount",
-                            "amount":20
-                          },
-                          {
-                            "name":"$10 Off Coupon",
-                            "amount":10
-                          }
-                        ]
+                         "buttons": [
+                            {
+                                "title": "View More",
+                                "type": "postback",
+                                "payload": "payload"                        
+                            }
+                        ]  
                     }
                 }
             };
