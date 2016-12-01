3f978f34e02bc148aba029f8e2521ffbba27c431
Mac <mac@Macs-MacBook-Pro.local>
2016-12-01 15:05:33 +0700

    new

--- index.js
+++ index.js
@@ -71,110 +71,74 @@ function kittenMessage(recipientId, text) {
                     "type": "template",
                     "payload": {
                         "template_type": "generic",
-                        // "elements": [
-                        //     {
-                        //         "title": "ICED VANILLA LATTE",
-                        //         "subtitle": "ICED VANILLA LATTE picture",
-                        //         "image_url": "http://againstallgrain.com/wp-content/uploads/2013/03/Iced_Vanilla_Coffee-006.jpg" ,
-                        //             "buttons": [{
-                        //                 "type": "web_url",
-                        //                 "url": "http://againstallgrain.com/wp-content/uploads/2013/03/Iced_Vanilla_Coffee-006.jpg",
-                        //                 "title": "Order"
-                        //                 }, {
-                        //                 "type": "postback",
-                        //                 "title": "details",
-                        //                 "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                        //             }],
-                        //         "title": "ICED LEMON TEA",
-                        //         "subtitle": "ICED LEMON TEA",
-                        //         "image_url": "http://leelalicious.com/wp-content/uploads/2016/04/Thai-Lemon-Iced-Tea.jpg" ,
-                        //             "buttons": [{
-                        //                 "type": "web_url",
-                        //                 "url": "http://leelalicious.com/wp-content/uploads/2016/04/Thai-Lemon-Iced-Tea.jpg",
-                        //                 "title": "Order"
-                        //                 }, {
-                        //                 "type": "postback",
-                        //                 "title": "details",
-                        //                 "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                        //             }],
-                        //         "title": "STRAWBERRY SMOOTHIES",
-                        //         "subtitle": "STRAWBERRY SMOOTHIES picture",
-                        //         "image_url": "http://foodrecipess.com/wp-content/uploads/2016/03/How-To-Make-Strawberry-Smoothie-Recipe..jpg" ,
-                        //             "buttons": [{
-                        //                 "type": "web_url",
-                        //                 "url": "http://machogrill63.ru/wp-content/uploads/2016/08/Hamburger-Food-Photography-Wallpaper-HD.jpg",
-                        //                 "title": "Order"
-                        //                 }, {
-                        //                 "type": "postback",
-                        //                 "title": "details",
-                        //                 "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                        //             }],
-                        //         "title": "STRAWBERRY CAKE",
-                        //         "subtitle": "STRAWBERRY CAKE picture",
-                        //         "image_url": "http://www.chelseasmessyapron.com/wp-content/uploads/2015/04/The-BEST-Strawberry-and-Cream-Cake-Cream-Cheese-Frosting.jpg" ,
-                        //             "buttons": [{
-                        //                 "type": "web_url",
-                        //                 "url": "http://www.chelseasmessyapron.com/wp-content/uploads/2015/04/The-BEST-Strawberry-and-Cream-Cake-Cream-Cheese-Frosting.jpg",
-                        //                 "title": "Order"
-                        //                 }, {
-                        //                 "type": "postback",
-                        //                 "title": "details",
-                        //                 "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                        //             }],
-                        //         "title": "CARAMEL LATTE",
-                        //         "subtitle": "CARAMEL LATTE",
-                        //         "image_url": "http://images2.laweekly.com/imager/demitasses-pistachio-rose-latte/u/745xauto/5299882/xmascoffee_demi01.jpg" ,
-                        //             "buttons": [{
-                        //                 "type": "web_url",
-                        //                 "url": "http://images2.laweekly.com/imager/demitasses-pistachio-rose-latte/u/745xauto/5299882/xmascoffee_demi01.jpg",
-                        //                 "title": "Order"
-                        //                 }, {
-                        //                 "type": "postback",
-                        //                 "title": "details",
-                        //                 "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                        //             }]
-                        //     }
-                        // ]
                         "elements": [
                             {
-                                "title": "Kitten",
-                                "subtitle": "Cute kitten picture",
-                                "image_url": imageUrl ,
+                                "title": "ICED VANILLA LATTE",
+                                "subtitle": "ICED VANILLA LATTE picture",
+                                "image_url": "http://againstallgrain.com/wp-content/uploads/2013/03/Iced_Vanilla_Coffee-006.jpg" ,
                                 "buttons": [{
                                     "type": "web_url",
-                                    "url": imageUrl,
-                                    "title": "Show kitten"
+                                    "url": "http://againstallgrain.com/wp-content/uploads/2013/03/Iced_Vanilla_Coffee-006.jpg",
+                                    "title": "Order"
                                     }, {
                                     "type": "postback",
-                                    "title": "I like this",
+                                    "title": "details",
+                                    "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                                }]
+                            },
+                            {     
+                                "title": "ICED LEMON TEA",
+                                "subtitle": "ICED LEMON TEA",
+                                "image_url": "http://leelalicious.com/wp-content/uploads/2016/04/Thai-Lemon-Iced-Tea.jpg" ,
+                                "buttons": [{
+                                    "type": "web_url",
+                                    "url": "http://leelalicious.com/wp-content/uploads/2016/04/Thai-Lemon-Iced-Tea.jpg",
+                                    "title": "Order"
+                                    }, {
+                                    "type": "postback",
+                                    "title": "details",
                                     "payload": "User " + recipientId + " likes kitten " + imageUrl,
                                 }]
                             },
                             {
-                                "title": "Kitten",
-                                "subtitle": "Cute kitten picture",
-                                "image_url": imageUrl ,
+                                "title": "STRAWBERRY SMOOTHIES",
+                                "subtitle": "STRAWBERRY SMOOTHIES picture",
+                                "image_url": "http://foodrecipess.com/wp-content/uploads/2016/03/How-To-Make-Strawberry-Smoothie-Recipe..jpg" ,
                                 "buttons": [{
                                     "type": "web_url",
-                                    "url": imageUrl,
-                                    "title": "Show kitten"
+                                    "url": "http://machogrill63.ru/wp-content/uploads/2016/08/Hamburger-Food-Photography-Wallpaper-HD.jpg",
+                                    "title": "Order"
                                     }, {
                                     "type": "postback",
-                                    "title": "I like this",
+                                    "title": "details",
+                                    "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                                }]
+                            },
+                            { 
+                                "title": "STRAWBERRY CAKE",
+                                "subtitle": "STRAWBERRY CAKE picture",
+                                "image_url": "http://www.chelseasmessyapron.com/wp-content/uploads/2015/04/The-BEST-Strawberry-and-Cream-Cake-Cream-Cheese-Frosting.jpg" ,
+                                "buttons": [{
+                                    "type": "web_url",
+                                    "url": "http://www.chelseasmessyapron.com/wp-content/uploads/2015/04/The-BEST-Strawberry-and-Cream-Cake-Cream-Cheese-Frosting.jpg",
+                                    "title": "Order"
+                                    }, {
+                                    "type": "postback",
+                                    "title": "details",
                                     "payload": "User " + recipientId + " likes kitten " + imageUrl,
                                 }]
                             },
                             {
-                                "title": "Kitten",
-                                "subtitle": "Cute kitten picture",
-                                "image_url": imageUrl ,
+                                "title": "CARAMEL LATTE",
+                                "subtitle": "CARAMEL LATTE",
+                                "image_url": "http://images2.laweekly.com/imager/demitasses-pistachio-rose-latte/u/745xauto/5299882/xmascoffee_demi01.jpg" ,
                                 "buttons": [{
                                     "type": "web_url",
-                                    "url": imageUrl,
-                                    "title": "Show kitten"
+                                    "url": "http://images2.laweekly.com/imager/demitasses-pistachio-rose-latte/u/745xauto/5299882/xmascoffee_demi01.jpg",
+                                    "title": "Order"
                                     }, {
                                     "type": "postback",
-                                    "title": "I like this",
+                                    "title": "details",
                                     "payload": "User " + recipientId + " likes kitten " + imageUrl,
                                 }]
                             }
