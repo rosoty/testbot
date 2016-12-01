636c3b8ab905cf70c121f0b311ba007f3a3bf5e3
Mac <mac@Macs-MacBook-Pro.local>
2016-12-01 11:23:53 +0700

    new

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.post('/webhook', function (req, res) {
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
             }
         } else if (event.postback) {
-            console.log("Postback received: " + JSON.stringify(event.postback));
+            //console.log("Postback received: " + JSON.stringify(event.postback));
+            receivedPostback(event);
         }
     }
     res.sendStatus(200);
@@ -97,4 +98,21 @@ function kittenMessage(recipientId, text) {
     return false;
     
 };
+
+function receivedPostback(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+
+    // The 'payload' param is a developer-defined field which is set in a postback 
+    // button for Structured Messages. 
+    var payload = event.postback.payload;
+
+    console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+    // When a postback is called, we'll send a message back to the sender to 
+    // let them know it was successful
+    sendMessage(senderID, "Postback called");
+}
 
