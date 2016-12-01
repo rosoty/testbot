7d646ef39ff4243b2a2ca13edaa9755291be1d25
Mac <mac@Macs-MacBook-Pro.local>
2016-12-01 12:29:34 +0700

    new

--- index.js
+++ index.js
@@ -67,24 +67,58 @@ function kittenMessage(recipientId, text) {
             var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
             
             message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Kitten",
-                            "subtitle": "Cute kitten picture",
-                            "image_url": "http://machogrill63.ru/wp-content/uploads/2016/08/Hamburger-Food-Photography-Wallpaper-HD.jpg" ,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": "http://machogrill63.ru/wp-content/uploads/2016/08/Hamburger-Food-Photography-Wallpaper-HD.jpg",
-                                "title": "Show kitten"
-                                }, {
-                                "type": "postback",
-                                "title": "I like this",
-                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                            }]
-                        }]
+                "attachment":{
+                    "type":"template",
+                    "payload":{
+                        "template_type":"receipt",
+                        "recipient_name":"Stephane Crozatier",
+                        "order_number":"12345678902",
+                        "currency":"USD",
+                        "payment_method":"Visa 2345",        
+                        "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+                        "timestamp":"1428444852", 
+                        "elements":[
+                          {
+                            "title":"Classic White T-Shirt",
+                            "subtitle":"100% Soft and Luxurious Cotton",
+                            "quantity":2,
+                            "price":50,
+                            "currency":"USD",
+                            "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+                          },
+                          {
+                            "title":"Classic Gray T-Shirt",
+                            "subtitle":"100% Soft and Luxurious Cotton",
+                            "quantity":1,
+                            "price":25,
+                            "currency":"USD",
+                            "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+                          }
+                        ],
+                        "address":{
+                          "street_1":"1 Hacker Way",
+                          "street_2":"",
+                          "city":"Menlo Park",
+                          "postal_code":"94025",
+                          "state":"CA",
+                          "country":"US"
+                        },
+                        "summary":{
+                          "subtotal":75.00,
+                          "shipping_cost":4.95,
+                          "total_tax":6.19,
+                          "total_cost":56.14
+                        },
+                        "adjustments":[
+                          {
+                            "name":"New Customer Discount",
+                            "amount":20
+                          },
+                          {
+                            "name":"$10 Off Coupon",
+                            "amount":10
+                          }
+                        ]
                     }
                 }
             };
