8110adcdddfc7e93d9edd426887fb03c9332ce73
Mac <mac@Macs-MacBook-Pro.local>
2016-12-01 10:28:11 +0700

    event kitten

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            if (!kittenMessage(event.sender.id, event.message.text)) {
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            }
         }
     }
     res.sendStatus(200);
@@ -94,8 +96,3 @@ function kittenMessage(recipientId, text) {
     
 };
 
-if (event.message && event.message.text) {
-    if (!kittenMessage(event.sender.id, event.message.text)) {
-        sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-    }
-}
