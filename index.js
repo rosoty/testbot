b6933c24d72e4a50627d1e35f0a36e23696181cd
Mac <mac@Macs-MacBook-Pro.local>
2016-12-02 15:57:33 +0700

    user info

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.post('/webhook', function (req, res) {
         var event = events[i];
         if (event.message && event.message.text) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
-                //sendMessage(event.sender.id, {text: "Echo: " + event.message.text + ", SenderId= " + event.sender.id });
-                getInfo(event.sender.id, {first_name: "Your name: " +event.message.first_name});
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.first_name + ", SenderId= " + event.sender.id });
+               // getInfo(event.sender.id, {first_name: "Your name: " +event.message.first_name});
             }
         } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
