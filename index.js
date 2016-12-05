fe1f171ad28936b12c720ef51bad6ad4e377fbd6
Mac <mac@Macs-MacBook-Pro.local>
2016-12-05 10:55:24 +0700

    web view

--- index.js
+++ index.js
@@ -83,7 +83,8 @@ function kittenMessage(recipientId, text) {
                                     {
                                         "type": "web_url",
                                         "url": "http://againstallgrain.com/wp-content/uploads/2013/03/Iced_Vanilla_Coffee-006.jpg",
-                                        "title": "Order"
+                                        "title": "Order",
+                                        "webview_height_ratio": "full"
                                     },
                                     {
                                         "type": "postback",
@@ -100,7 +101,8 @@ function kittenMessage(recipientId, text) {
                                     {
                                         "type": "web_url",
                                         "url": "http://leelalicious.com/wp-content/uploads/2016/04/Thai-Lemon-Iced-Tea.jpg",
-                                        "title": "Order"
+                                        "title": "Order",
+                                        "webview_height_ratio": "full"
                                     },
                                     {
                                         "type": "postback",
@@ -117,7 +119,8 @@ function kittenMessage(recipientId, text) {
                                     {
                                         "type": "web_url",
                                         "url": "http://machogrill63.ru/wp-content/uploads/2016/08/Hamburger-Food-Photography-Wallpaper-HD.jpg",
-                                        "title": "Order"
+                                        "title": "Order",
+                                        "webview_height_ratio": "full"
                                     },
                                     {
                                         "type": "postback",
@@ -134,7 +137,8 @@ function kittenMessage(recipientId, text) {
                                     {
                                         "type": "web_url",
                                         "url": "http://www.chelseasmessyapron.com/wp-content/uploads/2015/04/The-BEST-Strawberry-and-Cream-Cake-Cream-Cheese-Frosting.jpg",
-                                        "title": "Order"
+                                        "title": "Order",
+                                        "webview_height_ratio": "full"
                                     },
                                     {
                                         "type": "postback",
@@ -151,7 +155,8 @@ function kittenMessage(recipientId, text) {
                                     {
                                         "type": "web_url",
                                         "url": "http://images2.laweekly.com/imager/demitasses-pistachio-rose-latte/u/745xauto/5299882/xmascoffee_demi01.jpg",
-                                        "title": "Order"
+                                        "title": "Order",
+                                        "webview_height_ratio": "full"
                                     },
                                     {
                                         "type": "postback",
