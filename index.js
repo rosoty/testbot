8dde6da3f30c063626c81b716f7df1e7e7ef397f
Mac <mac@Macs-MacBook-Pro.local>
2016-12-07 14:51:47 +0700

    info

--- index.js
+++ index.js
@@ -80,54 +80,56 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        console.log("BABA_KH: "+JSON.stringify(event.message));
-        if(event.message && event.message.attachments && event.message.attachments[0].payload && event.message.attachments[0].type=="location"){
+// app.post('/webhook', function (req, res) {
+//     var events = req.body.entry[0].messaging;
+//     for (i = 0; i < events.length; i++) {
+//         var event = events[i];
+//         console.log("BABA_KH: "+JSON.stringify(event.message));
+//         if(event.message && event.message.attachments && event.message.attachments[0].payload && event.message.attachments[0].type=="location")
+//         {
             
-        }
-        else if ((event.message && event.message.quick_reply && event.message.quick_reply.payload)|| (event.postback && event.postback.payload)){
-            console.log("DROITE_KH");
-            if(event.postback)
-                var str=event.postback.payload;
-            else
-                var str=event.message.quick_reply.payload;
+//         }
+//         else if ((event.message && event.message.quick_reply && event.message.quick_reply.payload)|| (event.postback && event.postback.payload))
+//         {
+//             console.log("DROITE_KH");
+//             if(event.postback)
+//                 var str=event.postback.payload;
+//             else
+//                 var str=event.message.quick_reply.payload;
 
-            console.log("STRE+"+str);
-            if(str.includes("step1"))
-            {
+//             console.log("STRE+"+str);
+//             if(str.includes("step1"))
+//             {
                     
                 
-            }
-            else if(str.includes("step2"))
-            {
+//             }
+//             else if(str.includes("step2"))
+//             {
          
 
-            }
-            else if(str.includes("step3"))
-            {         
+//             }
+//             else if(str.includes("step3"))
+//             {         
             
-            }
-        }
-        else if(event.message && event.message.text)
-        {
-            console.log("MON SIMPLE TEXT");
-            var str=event.message.text;
-            console.log("TEXT:"+str);
-            if(str.toLowerCase()=="hi")
-            {
-                sendMessage(event.sender.id,{"text": "Welcome HI"});
-            }
-            else if(str.toLowerCase()=="admin")
-            {
-                sendMessage(event.sender.id,{"text": "Welcome ADMIN"});   
-            }
-            else{
-                sendMessage(event.sender.id,{"text": "Welcome "+ event.message.text});
-            }
-        }
-    }
-    res.sendStatus(200);
-});
+//             }
+//         }
+//         else if(event.message && event.message.text)
+//         {
+//             console.log("MON SIMPLE TEXT");
+//             var str=event.message.text;
+//             console.log("TEXT:"+str);
+//             if(str.toLowerCase()=="hi")
+//             {
+//                 sendMessage(event.sender.id,{"text": "Welcome HI"});
+//             }
+//             else if(str.toLowerCase()=="admin")
+//             {
+//                 sendMessage(event.sender.id,{"text": "Welcome ADMIN"});   
+//             }
+//             else{
+//                 sendMessage(event.sender.id,{"text": "Welcome "+ event.message.text});
+//             }
+//         }
+//     }
+//     res.sendStatus(200);
+// });
